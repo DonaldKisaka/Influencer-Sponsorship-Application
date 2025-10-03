e84ebebc562b4e29521e1f1aebdfd9979f16ec83
DonaldKisaka <kisakadonald1@gmail.com>
2025-10-03 19:24:05 +0300

    first commit

---
+++ influencer-sponsorship-application/src/main/java/com/example/influencer/sponsorship/application/InfluencerSponsorshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.influencer.sponsorship.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluencerSponsorshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluencerSponsorshipApplication.class, args);
+	}
+
+}
---
+++ influencer-sponsorship-application/src/test/java/com/example/influencer/sponsorship/application/InfluencerSponsorshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.influencer.sponsorship.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluencerSponsorshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
