6bd58468bb30ea965a5ab7d5a25155c86629a614
DonaldKisaka <kisakadonald1@gmail.com>
2025-10-05 11:27:25 +0300

    dto

--- influencer-sponsorship-application/src/main/java/com/example/influencer/sponsorship/application/dto/OfferDto.java
+++ influencer-sponsorship-application/src/main/java/com/example/influencer/sponsorship/application/dto/OfferDto.java
@@ -1,10 +1,19 @@
 package com.example.influencer.sponsorship.application.dto;
 
 import com.example.influencer.sponsorship.application.model.OfferStatus;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 
 public record OfferDto (
+        @NotNull(message = "Influencer Id cannot be null")
         Long influencerId,
+
+        @NotNull(message = "Brand Id cannot be null")
         Long brandId,
+
+        @NotNull(message = "Money Amount cannot be null")
+        @DecimalMin(value = "0.0", message = "Money Amount cannot be negative")
         Double moneyAmount,
+
         OfferStatus status
 ) {}
