54a09a7624df63b0666811fbc030953275e95d1c
DonaldKisaka <kisakadonald1@gmail.com>
2025-10-08 10:50:06 +0300

    model

--- influencer-sponsorship-application/src/main/java/com/example/influencer/sponsorship/application/model/Brand.java
+++ influencer-sponsorship-application/src/main/java/com/example/influencer/sponsorship/application/model/Brand.java
@@ -3,6 +3,7 @@ package com.example.influencer.sponsorship.application.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Brand {
 
     @Id
